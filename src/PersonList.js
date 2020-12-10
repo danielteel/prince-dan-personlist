3cef861e647ecea8e3cec81c5c9d3e00132ff8ff
Daniel Teel <daniel.teel.2@us.af.mil>
2020-12-10 12:42:10 -0700

    refactored personlist.js

--- src/PersonList.js
+++ src/PersonList.js
@@ -1,15 +1,7 @@
-import React from 'react';
-
-export default class PersonList extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return  (
-                    this.props.people.length > 0 ?
-                        this.props.people.map( person => <li>{person}</li>)  : 
-                        "There are no people to list"
-                ) 
-    }
+export default function PersonList( {people} ){
+    return  (
+                people.length > 0 ?
+                    people.map( person => <li>{person}</li>)  : 
+                    "There are no people to list"
+            ) 
 }
